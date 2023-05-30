298f404e9a7ddc3c16d9b6727758dd2c34a472a9
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-05-30 16:48:20 +0000

    w

---
+++ temp.py
@@ -0,0 +1,11 @@
+import time
+
+
+def qBasic(q):
+    while True:
+        print('⇥', end='\r')
+        time.sleep(1)
+        print(' ', end='\r')
+        time.sleep(1)
+    temp = input("\n⇥")
+        
--- zam.py
+++ zam.py
@@ -1,16 +1,38 @@
-import math, sys, random, json
+#python3 -m pip install --upgrade termcolor
+import math, sys, random, json, time
+from termcolor import colored, cprint
 
-pos = str(0)
+pos = float(0)
 
-while True:
+wpm = 65 #wpm
+def Type(t): #Word typing control
+    for l in t:
+        sys.stdout.write(l)
+        sys.stdout.flush()
+        time.sleep(random.random()*10.0/wpm)
+
+def qBasic(q):
+    while True:
+        print('⇥', end='\r')
+        time.sleep(1)
+        print(' ', end='\r')
+        time.sleep(1)
+
+        temp = input("\n⇥")
+
+while True: #Start Game
     temp = input("start game? y/n: ").lower()
 
     if temp == "yes" or temp == "y":
         break
     elif temp == "no" or temp == "n":
         sys.exit("Ok then")
-    print("Um what?")
+    Type("Um what?")
+
+print(pos)
 
 while True:
     if pos == 0:
-        print("You find yourself on a rocky path in front of a slightly weathered, red bricked house")
+        Type("You find yourself on a rocky path in front of a slightly weathered, red bricked house")
+        Type("\n1)Walk inside\n2)Continue path left\n3)Continue path right")
+        
