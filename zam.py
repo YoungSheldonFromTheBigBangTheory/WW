87f937a5a5eed6d7d917952298727b6e529ac08c
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-05-31 03:55:55 +0000

    h

--- zam.py
+++ zam.py
@@ -4,7 +4,7 @@ from termcolor import colored, cprint
 
 pos = float(0)
 
-wpm = 65 #wpm
+wpm = 85 #wpm
 def Type(t): #Word typing control
     for l in t:
         sys.stdout.write(l)
@@ -15,21 +15,35 @@ def qBasic():
     #while True:
     while True:
         temp = input("\n⇥   ")
-        match temp:
-            case "1":
-                pos = tempPos1
-                return pos
-            case "2":
-                pos = tempPos2
-                return pos
-            case "3":
-                pos = tempPos3
-                return pos    
-            case "4":
-                pos = tempPos4
-                return pos
-            case _:
-                print("\nInvalid")
+        if tempPos4 != 1989:
+            match temp:
+                case "1":
+                    pos = tempPos1
+                    return pos
+                case "2":
+                    pos = tempPos2
+                    return pos
+                case "3":
+                    pos = tempPos3
+                    return pos    
+                case "4":
+                    pos = tempPos4
+                    return pos
+                case _:
+                    print("\nInvalid")
+        if tempPos4 == 1989:
+            match temp:
+                case "1":
+                    pos = tempPos1
+                    return pos
+                case "2":
+                    pos = tempPos2
+                    return pos
+                case "3":
+                    pos = tempPos3
+                    return pos    
+                case _:
+                    print("\nInvalid")
 
 while True: #Start Game
     temp = input("start game? y/n: ").lower()
@@ -42,18 +56,35 @@ while True: #Start Game
 
 while True: #Pos 0
     
-    if pos == 0:
+    if pos == 0: #Spawn
         Type("You find yourself on a rocky path in front of a slightly weathered, red bricked house")
         Type("\n1)Walk inside\n2)Continue path left\n3)Continue path right")
-        tempPos1 = 1
-        tempPos2 = 0.1
-        tempPos3 = 0.2
+        tempPos1 = 1 #Walk inside
+        tempPos2 = 0.1 #Continue path left
+        tempPos3 = 0.2 #Continue path right
+        tempPos4 = 1989 #Does nothing
         pos = qBasic()
-    if pos == 1:
-        Type("You find yourself at the entrance of the house")
+    if pos == 1: #House Entrance
+        Type("You find yourself in the entrance of the house")
         Type("\n1)Head upstairs\n2)Head straight towards kitchen\n3)Turn right to the living room\n4)Exit the house")
-        tempPos1 = 1.2
-        tempPos2 = 1.11
-        tempPos2 = 1.12
-        tempPos4 = 0
+        tempPos1 = 1.2 #Head upstairs
+        tempPos2 = 1.11 #Head towards kitchen
+        tempPos3 = 1.12 #Turn right to living room
+        tempPos4 = 0 #Exit house
+        pos = qBasic()
+    if pos == 1.11: #Kitchen
+        Type("You step onto the black & white checkered floor. Close beside you to your left is a line of smooth white cabinets. ")
+        Type("\n1)Search cabinet\n2)Search drawer\n3)Turn right to the living room\n4)Return to entrance")
+        tempPos1 = 1.111 #Search cabinet
+        tempPos2 = 1.112 #Seach drawer
+        tempPos3 = 1.12 #Turn right to living room
+        tempPos4 = 1 #Return to entrance
+        pos = qBasic()
+    if pos == 1.12: #Living Room
+        Type("The hard wood creaks below you as you enter the living room. You aproach the window and look outside. It is dark.")
+        Type("\n1)Look outside\n2)Go to kitchen\n3)Return to entrance")
+        tempPos1 = 1.21 #Look outside
+        tempPos2 = 1.11 #Go to kitchen
+        tempPos3 = 1.12 #Return to entrance
+        tempPos4 = 1989 #Does nothing
         pos = qBasic()
