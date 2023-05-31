4572ba3d63568ae99190319e911eae20af1f6988
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-05-31 01:54:12 +0000

    h

--- zam.py
+++ zam.py
@@ -24,9 +24,11 @@ def qBasic():
             case "3":
                 pos = tempPos3
                 return pos    
+            case "4":
+                pos = tempPos4
+                return pos
             case _:
                 print("\nInvalid")
-        
 
 while True: #Start Game
     temp = input("start game? y/n: ").lower()
@@ -38,6 +40,7 @@ while True: #Start Game
     Type("Um what?")
 
 while True: #Pos 0
+    
     if pos == 0:
         Type("You find yourself on a rocky path in front of a slightly weathered, red bricked house")
         Type("\n1)Walk inside\n2)Continue path left\n3)Continue path right")
@@ -45,6 +48,13 @@ while True: #Pos 0
         tempPos2 = 0.1
         tempPos3 = 0.2
         qBasic()
+        pos = qBasic()
     if pos == 1:
         Type("You find yourself at the entrance of the house")
-        Type("\n1)Head upstairs\n2)Head straight kitchen\n3)Turn right to the living room")
+        Type("\n1)Head upstairs\n2)Head straight towards kitchen\n3)Turn right to the living room\n4)Exit the house")
+        tempPos1 = 1.2
+        tempPos2 = 1.11
+        tempPos2 = 1.12
+        tempPos4 = 0
+        qBasic()
+        pos = qBasic()
