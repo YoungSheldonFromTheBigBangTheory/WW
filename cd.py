e5c86b391096d8f95379f90ae72cba9ee764b0c2
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-06-09 16:15:01 +0000

    Commitmsg

--- cd.py
+++ cd.py
@@ -7,10 +7,17 @@ print(Inventory.inventory)
 x = Items.Spoon
 Inventory.inventory.append(x)
 
+print(*Inventory.inventory, sep = "\n")
+
 #x = input(str("input: "))
 #Inventory.inventory.append(x)
 
+#for item in Inventory.inventory:
+    #item:Item
+    #print(item, item.Check(Items.Spoon))
+
 for item in Inventory.inventory:
     item:Item
-    print(item, item.Check(Items.Spoon))
+    if item.Check(Items.Spoon) is True:
+        print("a")
    
